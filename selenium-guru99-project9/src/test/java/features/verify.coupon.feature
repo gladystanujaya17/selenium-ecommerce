5f9ac3443bf8459949c49953c01a416875ff5d6d
gladystanujaya17 <gladystana17@gmail.com>
2024-03-27 15:42:04 +0700

    Ninth project (revision)

--- selenium-guru99-project9/src/test/java/features/verify.coupon.feature
+++ selenium-guru99-project9/src/test/java/features/verify.coupon.feature
@@ -8,3 +8,11 @@ Feature: Verify discount coupon feature
 		Then User add iPhone to cart
 		When User enter coupon code
 		Then User verify the discount is generated
+		
+	@negative
+  Scenario: Failed to verify discount coupon in Magento
+		Given User is on Magento page
+		When User go to Mobile page
+		Then User add iPhone to cart
+		When User enter different coupon code
+		Then User verify the discount is not generated
--- selenium-guru99-project9/src/test/java/steps/MagentoCoupon.java
+++ selenium-guru99-project9/src/test/java/steps/MagentoCoupon.java
@@ -1,5 +1,7 @@
 package steps;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -14,6 +16,7 @@ import cucumber.api.java.en.When;
 public class MagentoCoupon {
 
 	WebDriver driver;
+    String keys = "GURU99";
 	
 	@Given("User is on Magento page")
 	public void userIsOnMagentoPage() {
@@ -46,6 +49,26 @@ public class MagentoCoupon {
 	    String discount = driver.findElement(By.xpath("//*[@id=\"shopping-cart-totals-table\"]/tbody/tr[2]/td[2]/span")).getText();
 	    System.out.println("Discount: " + discount);
 	    Thread.sleep(2000);
+	    driver.quit();
+	}
+	
+	@When("User enter different coupon code")
+	public void userEnterDifferentCouponCode() {
+		WebElement couponCode = driver.findElement(By.xpath("//*[@id=\"coupon_code\"]"));
+		couponCode.sendKeys(keys);
+	    driver.findElement(By.xpath("//*[@id=\"discount-coupon-form\"]/div/div/div/div/button")).click();
+	}
+	
+	@Then("User verify the discount is not generated")
+	public void userVerifyTheDiscountIsNotGenerated() throws InterruptedException {
+		String errorMsg1 = "Coupon code \"GURU99\" is not valid.";
+		String errorMsg2 = driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div/div/ul/li/ul/li/span")).getText();
+		try {
+			assertEquals(errorMsg1, errorMsg2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Thread.sleep(2000);
 	    driver.quit();
 	}
 }
